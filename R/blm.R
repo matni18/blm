87f5a38f92098f2abd28c6152b09f8f75d807859
matni18 <matildenielsen86@hotmail.com>
2016-12-13 13:05:32 +0100

    Matilde Nielsen
    implemented blm and predict

--- R/blm.R
+++ R/blm.R
@@ -8,7 +8,24 @@
 #'
 #' @return A fitted model.
 #' @export
-blm <- function(model, ...) {
-  # implement your function here...
+
+
+blm <- function(model, alpha, beta, data, ...) {
+  phiX = model.matrix(model, data)
+  Sxy = solve(diag(alpha, nrow=ncol(phiX)) + beta * t(phiX) %*% phiX)
+  mxy = beta * Sxy %*% t(phiX) %*% model.response(model.frame(model, data))
+  structure(list(sigma = Sxy, mean = mxy, data = data, model = model), class = "blm")
+}
+#Test:
+#test =blm(y ~ x+I(x^2), 1,1,data.frame(x=x,y=y))
+#predict(test)
+
+predict.blm <- function(blmModel) {
+  phiX = model.matrix(blmModel$model, blmModel$data)
+  mxy = blmModel$mean
+  Sxy = blmModel$sigma
+
+  apply(phiX, 1, function(row) t(mxy)%*%row)
+
 }
 
